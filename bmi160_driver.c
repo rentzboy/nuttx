74605fe56aee0cfef56cb8ae0b788d648bb9687e
tony <airanzo.emil@gmail.com>
2025-04-14 02:12:12 +0200

    nothing

--- bmi160_driver.c
+++ bmi160_driver.c
@@ -1,4 +1,4 @@
-//FS
+//--------------FS --------------//
 struct file {
     int flags;
     int pos;
@@ -35,12 +35,12 @@ int register_driver(char *devName, struct file_operations *f_ops, int mode, void
     if(i==NULL)
         return 1;
     i->i_name = devName; //dev/i2c1
-    i->i_priv = priv;    //xxx_drvr_s
-    i->u = f_ops;        //&xxx
-    i->flags = mode;     //0608
+    i->u = f_ops;        //&g_i2c_drvr_ops
+    i->flags = mode;     //0666 for char devices
+    i->i_priv = priv;    //i2c_master_s
 }
 
-//I2C
+//-------------- I2C --------------//
 struct i2c_drvr_s
 {
     struct i2c_master_s *i2c;
@@ -49,31 +49,35 @@ struct i2c_drvr_s
     bool unlinked;
 };
 
-struct file_ops_s g_i2c_drvr_ops {
-    i2c_drvr_open;
-    i2c_drvr_close;
-    i2c_drvr_read;
-    i2c_drvr_write;
-    i2c_drvr_ioctl;
+struct file_ops_s g_i2c_drvr_ops = {
+    i2c_drvr_open,
+    i2c_drvr_close,
+    i2c_drvr_read,
+    i2c_drvr_write,
+    i2c_drvr_ioctl
 };
 
-//I2C-master
+//-------------- I2C-master --------------//
 struct i2c_master_s {
     struct i2c_ops_s *ops;
 };
 
 struct i2c_ops_s {
-    (*transfer) ();
-    (*setup) (struct i2c_master_s *dev);
+    (*transfer) (struct i2c_master_s *dev, struct i2c_msg_s *msgs, int count);
+    (*setup) (struct i2c_master_s *dev, struct config_s *config);
+    (*reset) (struct i2c_master_s *dev);
+    (*shutdown) (struct i2c_master_s *dev);
 };
 
 int i2c_register(struct i2c_master_s *i2c, int bus){
-    const char *devName = "/dev/i2c1"; //generar el devPath
+    const char *devName = "/dev/i2c" & "bus"; //generar el devPath
     //register_driver(char *devName, struct file_operations *f_ops, int mode, char *priv)
-    register_driver(devName, &g_i2c_drvr_ops, 066, i2c);
+    register_driver(devName, &g_i2c_drvr_ops, 0666, i2c);
+    //open, close, read, write => i2c_drvr_open, i2c_drvr_close, i2c_drvr_read, i2c_drvr_write
+    //transfer, setup => i2c_transfer, i2c_setup
 }
 
-//BMI160
+//-------------- BMI160 --------------//
 struct bmi160_dev_s {
     struct i2c_master_s *i2c;
     int address;
@@ -87,8 +91,18 @@ struct bmi160_fops_s {
     int (*bmi160_write)(struct file* filep, char *buffer, int buflen);
 };
 
-bmi160_register(struct bmi160_dev_s *dev, struct i2c_master_s *i2c){
+int bmi160_register(const char *devpath, struct i2c_master_s *dev){
+    //Hay que crear el file device + el inode asociado
+    struct inode *i_node = kmalloc(sizeof(struct inode));
+    i_node->i_name = devpath;
+    i_node->u = &file_ops_s;
+
+    struct bmi160_dev_s *dev;
+    
+    
+
     //driver_register(char *devName, struct file_operations *f_ops, int mode, char *priv)
+    driver_register(devpath, &g_bmi160_fops, 0666, dev);
 
 }
 
@@ -106,6 +120,3 @@ int bmi160_read(int fd, char* buffer, int lenght){
 int bmi160_write(int fd, char* buffer, int lenght){
     //
 }
-
-
-
--- i2cX.c
+++ i2cX.c
@@ -4,6 +4,22 @@
 #include "stm32_gpio.h"
 #include "stm32_i2c.h"
 
+#define SETUPI2C(i2c, b, c)       \
+    do                            \
+    {                             \
+        (i2c)->hz = (a);          \
+        (i2c)->address = (b);     \
+        i2c->addressLenght = (c); \
+    } while (0); //execute 1 time only
+    
+#define TRANSFERI2C(i2c, b, c)    \
+    do                            \
+    {                             \
+        (i2c)->hz = (a);          \
+        (i2c)->address = (b);     \
+        i2c->addressLenght = (c); \
+    } while (0); //execute 1 time only
+
 //Struct declaration
 struct cfg {
     int address;
@@ -28,6 +44,9 @@ struct cfg cfg = {
     .frequence = 400
 };
 
+//Struct initialization type2
+struct cfg cfg = {0x87, 8, 400};
+
 static struct i2cInit {
     int filterAN;   //Analog filter
     int filterDI;   //Digital filter
@@ -35,26 +54,21 @@ static struct i2cInit {
     int addressDev; //Device datasheet
     int timing;
 
-}g_i2cInit; //still accessible using pointers from outside file ? no creo .... habría que declararla en el header
+}g_i2cInit;
 
-g_i2cInit = {0, 0, 7, 0, 0}; //Default values
 
-//Struct initialization type2
-struct cfg cfg = {0x87, 8, 400};
-
-int i2c_register(struct i2c_master_s *device, int bus) {
+int i2c_register(struct i2c_master_s *i2c_master_dev, int bus) {
+    //Clocks config.
+    i2c_Clocks_Initialization(bus);
     //GPIO config.
-    i2c2_initialization(bus);
+    i2c_GPIO_Initialization(bus);
     //Driver register
-    register_driver("devName", &g_i2c_drvr_ops, 0666, device);
+    register_driver("devName", &g_i2c_drvr_ops, 0666, i2c_master_dev);
     //I2C config.
-
-
-
-    i2c_setup(cfg_s, dev);
+    i2c_Initialization(int bus);
 }
 
-int i2c2_clocksInitialization(int bus) {
+int i2c_Clocks_Initialization(int bus) {
     //Clock enable APB1 -I2C bus clock-
     uint32_t regval;
     
@@ -79,15 +93,27 @@ int i2c2_clocksInitialization(int bus) {
     putreg32(regval, STM32_RCC_AHBENR);
 }
 
-int i2c2_initialization(int bus) {
-    uint32_t regval;
+int i2c_GPIO_Initialization(int bus) {
     //GPIO configuration: SDA pin 11 | SCL pin 9
     stm32_configgpio(GPIO_I2C2_SCL_1); //(GPIO_ALT|GPIO_AF4|GPIO_SPEED_50MHz|GPIO_OPENDRAIN|GPIO_PORTA|GPIO_PIN9) -pinmap legacy-
     stm32_configgpio(GPIO_I2C2_SDA_1); //(GPIO_ALT|GPIO_AF4|GPIO_SPEED_50MHz|GPIO_OPENDRAIN|GPIO_PORTA|GPIO_PIN10) -pinmap legacy-
+}
 
-    //Mode, Timing, Filters, Address, ACK
-    //Filters: I2C_CR1_ANFOFF & I2C_CR1_DNF from Control register 1
-    //10 bits address: I2C_CR2_ADD10 from Control register 2
+int i2c_Initialization(int bus) {
+    /* Master initialization (ver table 295. I2C initialization flowchart pág. 816 Reference Manual)
+    [parámetros dependientes del usuario: únicamente la frequencia]
+    1- Clear PE bit in I2C_CR1 register
+    2- Configure Analog filter ANOFF and Digital filter DNF bits in I2C_CR1 register
+    3- Configure PRESC[3:0], SDADEL[3:0], SCLDEL[3:0] SCLL[7:0], SCLH[7:0] bits in I2C_TIMINGR register
+       utilizar STM32 Excel tool calculator/table with predefined values
+    4- Configure NOSTRETCH bit in I2C_CR1 register
+    5- Set PE bit in I2C_CR1 register
+
+    //Acknowledge: ACK bit from Control register 2                      => initTypeDef
+    //NACK: NACK bit from Control register 2                            => initTypeDef
+    //PEC: PEC bit from Control register 2                              => initTypeDef
+    //Enable Interrupts and/or DMA in I2C_CR1 register */
+
     if(g_i2cInit.addressLen = 7) 
     {
         //
@@ -104,58 +130,38 @@ int i2c2_initialization(int bus) {
     {
 
     }
-    
-    //Timing: Timeout registers 1 & 2 ..... no se como se configura
-
-}
-
-#define SETUPI2C(i2c, b, c)       \
-    do                            \
-    {                             \
-        (i2c)->hz = (a);          \
-        (i2c)->address = (b);     \
-        i2c->addressLenght = (c); \
-    } while (0); //execute 1 time only
-    
-#define TRANSFERI2C(i2c, b, c)    \
-    do                            \
-    {                             \
-        (i2c)->hz = (a);          \
-        (i2c)->address = (b);     \
-        i2c->addressLenght = (c); \
-    } while (0); //execute 1 time only
-
-int i2c_setup(struct i2c_cfg, struct i2c_master_s *dev) {
-/*  Master initialization
-    Addressing mode (7-bit or 10-bit): ADD10 in I2C_CR2 register
-    Slave address to be sent: SADD[9:0] in I2C_CR2 register
-    Transfer direction: RD_WRN in I2C_CR2 register => no entiendo por qué aqui .....
-    The number of bytes to be transferred: NBYTES[7:0] in I2C_CR2 register
-    If the number of bytes is equal to or greater than 255 bytes, NBYTES[7:0] = 0xFF
-    You must then set the START bit in I2C_CR2 register.
-    
-
-    Enable Interrupts and/or DMA in I2C_CR1 register
-    */
 
     //Set the PE bit in the I2C_CR1 register and interrupts
     regval = getreg32(STM32_I2C_CR1);
     regval |= (I2C_CR1_TXIE | I2C_CR1_ADDRIE | I2C_CR1_PE); //TX, RX Interrup Enable, Peripheral Enable
     putreg32(regval, STM32_RCC_AHBENR);
+}
 
-};
+int i2c_setup(struct i2c_cfg, struct i2c_master_s *dev) {
+    //POR EL MOMENTO ES LO MISMO QUE I2C_Initialization
+}
 
 int i2c_transfer(uint8_t *data, int8_t register) {
-    //
+    /* Master PRE-transfer:
+    [parámetros dependientes del usuario: addressing mode, slave address, ¿transfer direction?, number of bytes]
+    1- Addressing mode (7-bit or 10-bit): ADD10 in I2C_CR2 register      => i2c_cfg
+    2- Slave address to be sent: SADD[9:0] in I2C_CR2 register           => i2c_cfg
+    3- Transfer direction: RD_WRN bit from Control register 2            => f(read | write)
+    4- Number of bits to transfer: NBYTES[7:0] from Control register 2   => i2c_msg
+       If the number of bytes is equal to or greater than 255 bytes, NBYTES[7:0] = 0xFF,
+       You must then set the START bit in I2C_CR2 register.
+    6- Start: START bit from Control register 2                          => from code
+    7- PE Enable: PE bit from Control register 2                         => from code
+    8- STOP: STOP bit from Control register 2                            => from code 
+    */
 }
 
 int i2c_read(int8_t addr, int8_t reg, uint8_t *data, int8_t len) {
-    //Always from master (µC) to slave (sensor)
-
-
+    //Master receiver mode
+    
 }
 
 int i2c_write() {
-    //
+    //Master transmitter mode
 
 }
