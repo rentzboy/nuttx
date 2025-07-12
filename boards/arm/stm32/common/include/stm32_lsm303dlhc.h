93c1b2e3046b72cd4a80f63852f5acc07daeefd8
rentzboy <airanzo.emil@gmail.com>
2025-07-12 19:23:28 +0200

    correct pins for the LSM303DLHC in the STM32F3Discovery

---
+++ boards/arm/stm32/common/include/stm32_lsm303dlhc.h
@@ -0,0 +1,83 @@
+/****************************************************************************
+ * boards/arm/stm32/common/include/lsm303dlhc.h
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __BOARDS_ARM_STM32_COMMON_INCLUDE_STM32_LSM303DLHC_H
+#define __BOARDS_ARM_STM32_COMMON_INCLUDE_STM32_LSM303DLHC_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Type Definitions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Types
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+#ifdef __cplusplus
+#define EXTERN extern "C"
+extern "C"
+{
+#else
+#define EXTERN extern
+#endif
+
+/****************************************************************************
+ * Inline Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+/**
+ * stm32_lsm303dlhc_initialize - Initialize the LSM303DLHC sensor using the I2C
+ * interface of the specified bus
+ *
+ * @param bus The bus number of the I2C interface to use
+ *
+ * @return 0 on success, -1 on failure
+ *
+ * This function allocates memory for the LSM303DLHC device structure, initializes
+ * the device addresses and frequency, and registers the device driver with the
+ * system using the provided I2C interface.
+ */
+ static int stm32_lsm303dlhc_initialize(int bus)
+
+#undef EXTERN
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __BOARDS_ARM_STM32_COMMON_INCLUDE_STM32_BMP180_H */
---
+++ boards/arm/stm32/common/src/stm32_lsm303dlhc.c
@@ -0,0 +1,100 @@
+/****************************************************************************
+ * boards/arm/stm32/common/src/stm32_lsm303dlhc.c
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+#include <nuttx/arch.h>
+#include <nuttx/i2c/i2c_master.h>
+#include <stdio.h>
+#include <debug.h>
+#include "stm32_i2c.h"
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Types
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Function Prototypes
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+
+
+/**
+ * stm32_lsm303dlhc_initialize - Initialize the LSM303DLHC sensor using the I2C
+ * interface of the specified bus
+ *
+ * @param bus The bus number of the I2C interface to use
+ *
+ * @return 0 on success, -1 on failure
+ *
+ * This function allocates memory for the LSM303DLHC device structure, initializes
+ * the device addresses and frequency, and registers the device driver with the
+ * system using the provided I2C interface.
+ */
+ static int stm32_lsm303dlhc_initialize(int bus)
+{
+  struct i2c_master_s *i2c;
+  int ret;
+    sninfo("Initializing lsm303dlhc!\n");
+
+  //Get the I2C interface registered for the bus  
+  i2c = stm32_i2c_get_instance(bus);
+  if (i2c == NULL)
+    {
+      syslog(LOG_ERR, "ERROR: Failed to get an instance of I2C%d interface\n", bus);
+      return -1;
+    }
+  else
+    {
+      ret = register_lsm303dlhc(i2c);
+      if (ret < 0)
+        {
+          syslog(LOG_ERR, "ERROR: Failed to register the LSM303DLHC sensor with the I2C%d interface\n",
+                 bus);
+
+        }
+    }
+    return ret;
+}
